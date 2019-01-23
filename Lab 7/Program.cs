222a3f1c5823e225e72628693580141952c333b1
stevojed <jedi1014@comcast.net>
2019-01-22 20:32:51 -0500

    Added Formating

--- Lab 7/Program.cs
+++ Lab 7/Program.cs
@@ -13,8 +13,7 @@ namespace Lab_7
         public static List<string> validPass = new List<string>();
 
         static void Main()
-        {
-            
+        {            
             bool run = true;
             while (run == true)
             {                
@@ -22,18 +21,18 @@ namespace Lab_7
                 string userName = Console.ReadLine();
                 bool user = UserName(userName);
 
-                 if (user == true)
-                    {
-                        Console.WriteLine(userName + " is valid.");
+                if (user == true)
+                   {
+                        Console.WriteLine("\n"+ userName + " is valid.\n");
                         validEmail.Add(userName);
                         run = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine(userName + " is an invalid email.");
-                    }
-                }
+                   }
+                   else
 
+                   {
+                        Console.WriteLine("\n" + userName + " is an invalid email.\n");
+                   }
+            }
             bool run2 = true;
             while (run2 == true)
             {
@@ -43,20 +42,22 @@ namespace Lab_7
                 bool pass = Password(password);
                 if (pass == true)
                 {
-                    Console.WriteLine(password + " is valid.");
+                    Console.WriteLine("\n" + password + " is valid.\n");
                     validPass.Add(password);
                     run2 = false;
                 }
                 else
                 {
-                    Console.WriteLine(password + " is an invalid password.");
+                    Console.WriteLine("\n" + password + " is an invalid password.\n");
                 }
             }
             Continue();
+            Console.WriteLine("\n\nGood Bye!");
+            Console.ReadKey();
         }
         public static bool UserName(string userName)
         {
-            string name = "^[a-zA-Z0-9]{3,11}[@][a-zA-Z]{3,11}[.][a-zA-Z]{2,3}$";
+            string name = "^[a-zA-Z0-9]{3,11}[@][a-zA-Z0-9]{3,11}[.][a-zA-Z]{2,3}$";
             Regex test = new Regex(name);
             return test.IsMatch(userName);
         }
@@ -80,10 +81,10 @@ namespace Lab_7
             }
             else if (input == "n")
             {
-                Console.WriteLine("Email Address\t\tPassword\n=============\t\t==========\n");
+                Console.WriteLine("\n{0,-36}{1,-36}\n{2,-36}{2,-36}\n","       Email","       Password","=====================");
                 for ( int i = 0; i < validEmail.Count; i++)
-                {  
-                    Console.WriteLine(validEmail[i] + "\t\t" + validPass[i]);
+                {
+                    Console.WriteLine("\n{2}: {0,-36}{2}: {1,-34}", validEmail[i], validPass[i], i+1);                    
                     cont = false;
                 }
             }
